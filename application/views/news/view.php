a405f5066c3cf08c0d7b04ecd20f524a77032f45
Myke <myke.agonia@gmail.com>
2017-09-15 20:45:03 +0800

    Create white container for main contents

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,3 +1,9 @@
-<?php
-echo '<h2>'.$news_item['title'].'</h2>';
-echo $news_item['text'];
+
+<div class="w3-container">
+    <div class="w3-ul w3-card-4 w3-white">
+        <?php
+            echo '<h2>'.$news_item['title'].'</h2>';
+            echo $news_item['text'];
+        ?>
+    </div>
+</div>
