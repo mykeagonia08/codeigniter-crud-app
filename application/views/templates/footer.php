758ab2f829a6b8e791fb0ccfd754e772fd35a3ee
Myke <myke.agonia@gmail.com>
2017-09-15 20:45:35 +0800

    restructure view templates

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,7 @@
+                <footer class="w3-container">
+                    <h4>Codeigniter CRUD Application</h4>
+                    <p>Copyright <em>&copy; 2017</em> <a href="https://github.com/mykeagonia08/" target="_blank">Myke Agonia</a></p>
+                </footer>
             </div>
-        <em>&copy; 2015</em>
     </body>
 </html>
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,4 +1,5 @@
-<nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;display:none;" id="mySidebar"><br>
   <div class="w3-container w3-row">
     <div class="w3-col s4">
       <img src="https://www.w3schools.com/w3images/avatar2.png" class="w3-circle w3-margin-right" style="width:46px">
@@ -21,3 +22,31 @@
     <a href="<?php echo site_url('News/index') ?>" class="w3-bar-item w3-button w3-padding"><i class="fa fa-newspaper-o "></i>&nbsp; News</a>
   </div>
 </nav>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor: pointer; display: none;" title="close side menu" id="myOverlay"></div>
+
+<script>
+    // Get the Sidebar
+    var mySidebar = document.getElementById("mySidebar");
+
+    // Get the DIV with overlay effect
+    var overlayBg = document.getElementById("myOverlay");
+
+    // Toggle between showing and hiding the sidebar, and add overlay effect
+    function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+        overlayBg.style.display = "none";
+    } else {
+        mySidebar.style.display = 'block';
+        overlayBg.style.display = "block";
+    }
+    }
+
+    // Close the sidebar with the close button
+    function w3_close() {
+    mySidebar.style.display = "none";
+    overlayBg.style.display = "none";
+    }
+</script>
